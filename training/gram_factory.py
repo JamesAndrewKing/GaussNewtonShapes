6b4a5adec635a5c7145a758d1666b2d8b131dc1f
James King <jaking@eu-lo-g3-010.euler.ethz.ch>
2025-04-11 18:01:18 +0200

    added Woodbury

--- training/gram_factory.py
+++ training/gram_factory.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 # Calculate Gauss-Newton Gram Matrices
 
 def calculate_A_interface(model, pts):
@@ -35,3 +36,61 @@ def compute_gram_matrix(model, config):
         A += loss_weights["curvature"] * calculate_A_mean_curvature(model, pts_surface)
 
     return A.detach()
+
+
+
+@torch.no_grad()
+def compute_jacobian_and_residual(model, config):
+    def flatten_grad_dict(grad_dict):
+        return torch.cat([p.flatten(start_dim=1) for p in grad_dict.values()], dim=1)
+
+    loss_weights = config["loss_weights"]
+    params = model.params
+
+    J_blocks = []
+    r_blocks = []
+
+    # Interface
+    if "pts_boundary" in config and loss_weights.get("interface", 0.0) > 0:
+        pts = config["pts_boundary"]
+        N = pts.shape[0]
+        r = model.f(params, pts).squeeze(1)
+        r_blocks.append(
+            np.sqrt(loss_weights["interface"] / N) * r
+        )
+        dr = flatten_grad_dict(model.grad_theta_f(params, pts))
+        J_blocks.append(
+            np.sqrt(loss_weights["interface"] / N) * dr
+        )
+
+    # Eikonal
+    if "pts_eikonal" in config and loss_weights.get("eikonal", 0.0) > 0:
+        pts = config["pts_eikonal"]
+        N = pts.shape[0]
+        r = model.r_eikonal(params, pts).squeeze(1)
+        r_blocks.append(
+            np.sqrt(loss_weights["eikonal"] / N) * r
+        )
+        dr = flatten_grad_dict(model.grad_theta_r_eikonal(params, pts))
+        J_blocks.append(
+            np.sqrt(loss_weights["eikonal"] / N) * dr
+        )
+
+    # Curvature
+    if "pts_surface" in config and loss_weights.get("curvature", 0.0) > 0:
+        pts = config["pts_surface"]
+        N = pts.shape[0]
+        r = model.r_mean_curvature(params, pts).squeeze(1)
+        r_blocks.append(
+            np.sqrt(loss_weights["curvature"] / N) * r
+        )
+        dr = flatten_grad_dict(model.grad_theta_r_mean_curvature(params, pts))
+        J_blocks.append(
+            np.sqrt(loss_weights["curvature"] / N) * dr
+        )
+
+    # Stack full residual and Jacobian
+    r = torch.cat(r_blocks, dim=0)
+    J = torch.cat(J_blocks, dim=0).T
+
+    return J.detach(), r.detach()
--- training/optimizers.py
+++ training/optimizers.py
@@ -1,6 +1,6 @@
 import torch
 from torch.nn.utils import parameters_to_vector, vector_to_parameters
-from .gram_factory import compute_gram_matrix
+from .gram_factory import compute_gram_matrix, compute_jacobian_and_residual
 
 class GaussNewton:
     def __init__(self, model, lr, config):
@@ -47,3 +47,35 @@ class GaussNewton:
                 continue
 
             param.data -= self.lr * param.grad
+
+
+class GaussNewtonWoodbury:
+    def __init__(self, model, lr, config):
+        self.model = model
+        self.params_dict = dict(model.named_parameters())
+        self.params = list(model.parameters())
+        self.lr = lr
+        self.t = 0
+        self.config = config
+
+    def zero_grad(self):
+        for p in self.params:
+            if p.grad is not None:
+                p.grad.zero_()
+
+    def calculate_update(self):
+        J, r = compute_jacobian_and_residual(self.model, self.config)
+        eps = self.config.get("regularization")
+        A = J.T @ J + eps*torch.eye(r.shape[0])
+        x, _, _, _ = torch.linalg.lstsq(A.double(), r.double(), driver="gels")
+        return J@x
+
+    @torch.no_grad()
+    def step(self):
+        self.t += 1
+
+        theta = parameters_to_vector(self.params)
+        theta -= self.lr * self.calculate_update()
+        vector_to_parameters(theta, self.params)
+        
+        
